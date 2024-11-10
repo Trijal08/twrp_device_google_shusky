d622703dd3fc0bc3c69a0d6ea49c48140982fdb7
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-11-10 17:04:38 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -80,9 +80,9 @@ PRODUCT_PACKAGES += \
 #     android.hardware.boot-service.default-pixel \
 #     android.hardware.boot-service.default_recovery-pixel
 
-PRODUCT_PACKAGES += \
-     android.hardware.health-service.zuma \
-     android.hardware.health-service.zuma_recovery
+#PRODUCT_PACKAGES += \
+#     android.hardware.health-service.zuma \
+#     android.hardware.health-service.zuma_recovery
 
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service-pixel \
